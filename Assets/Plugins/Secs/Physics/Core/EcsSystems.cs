6bdd290cb6d22c72115ee84140f8c4841eddb8dd
CheeseCake <grabarzpolski@gmail.com>
2023-05-27 16:17:55 +0200

    physics added

---
+++ Assets/Plugins/Secs/Physics/Core/EcsSystems.cs
@@ -0,0 +1,17 @@
+﻿using Secs.Physics;
+
+namespace Secs
+{
+    public sealed partial class EcsSystems
+    {
+        public void AttachPhysics()
+        {
+            EcsPhysics.BindToEcsWorld(_world,this);
+        }
+
+        public void ReleasePhysics()
+        {
+            EcsPhysics.UnbindToEcsWorld();
+        }
+    }
+}
---
+++ Assets/Plugins/Secs/Physics/Sys/DisposeOnTickPhysicsSys.cs
@@ -0,0 +1,55 @@
+﻿namespace Secs.Physics
+{
+    public sealed class DisposeOnTickPhysicsSys : IEcsRunSystem
+    {
+        [EcsInject] private EcsWorld _ecsWorld;
+        
+        [EcsInject(typeof(OnTriggerEnterEvent))]
+        private EcsFilter _onTriggerEnterFilter;
+        [EcsInject(typeof(OnTriggerStayEvent))]
+        private EcsFilter _onTriggerStayEventFilter;
+        [EcsInject(typeof(OnTriggerExitEvent))]
+        private EcsFilter _onTriggerExitEventFilter;
+
+        [EcsInject(typeof(OnCollisionEnterEvent))]
+        private EcsFilter _onCollisionEnterEventFilter;
+        [EcsInject(typeof(OnCollisionStayEvent))]
+        private EcsFilter _onCollisionStayEventFilter;
+        [EcsInject(typeof(OnCollisionExitEvent))]
+        private EcsFilter _onCollisionExitEventFilter;
+        
+        [EcsInject]
+        private EcsPool<OnTriggerEnterEvent> _onTriggerEnterEvent;
+        [EcsInject]
+        private EcsPool<OnTriggerStayEvent> _onTriggerStayEvent;
+        [EcsInject]
+        private EcsPool<OnTriggerExitEvent> _onTriggerExitEvent;
+        
+        [EcsInject]
+        private EcsPool<OnCollisionEnterEvent> _onCollisionEnterEvent;
+        [EcsInject]
+        private EcsPool<OnCollisionStayEvent> _onCollisionStayEvent;
+        [EcsInject]
+        private EcsPool<OnCollisionExitEvent> _onCollisionExitEvent;
+        public void OnRun()
+        {
+            foreach (var entity in _onTriggerEnterFilter)
+                _ecsWorld.DelEntity(entity);
+            
+            foreach (var entity in _onTriggerStayEventFilter)
+                _ecsWorld.DelEntity(entity);
+            
+            foreach (var entity in _onCollisionExitEventFilter)
+                _ecsWorld.DelEntity(entity);
+
+            foreach (var entity in _onCollisionEnterEventFilter)
+                _ecsWorld.DelEntity(entity);
+            
+            foreach (var entity in _onCollisionStayEventFilter)
+                _ecsWorld.DelEntity(entity);
+            
+            foreach (var entity in _onCollisionExitEventFilter)
+                _ecsWorld.DelEntity(entity);
+        }
+    }
+}
