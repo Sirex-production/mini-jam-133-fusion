e9fd1f4e1b782f767b7f361a7066ad5fb658104a
CheeseCake <grabarzpolski@gmail.com>
2023-05-29 00:59:01 +0200

    music added

---
+++ Assets/Source/Ecs/Audio/PlayMusic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+using Zenject;
+
+namespace Ingame.Audio
+{
+    public sealed class PlayMusic : MonoBehaviour
+    {
+        [SerializeField, Range(0, 1)] private float musicVolume;
+        [Required, SerializeField] private AudioClip musicClip;
+        private SoundService _soundService;
+        
+        [Inject]
+        private void Construct(SoundService soundService)
+        {
+            _soundService = soundService;
+        }
+
+        private void Start()
+        {
+           var audioSource = _soundService.PlaySound(musicClip, true);
+           audioSource.volume = musicVolume;
+        }
+    }
+}
